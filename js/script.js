2c2849462577e4f2ff07c07e5d7f1f59d9c5830f
ArtPlatosha <zipex98@gmail.com>
2023-04-24 22:03:23 +0300

    commit: 20th commit
    adding review-slider (JS)

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () => {
 	loginForm.classList.toggle('active');
 }
+
+var swiper = new Swiper('.review-slider', {
+	spaceBetween: 20,
+	centeredSliders: true,
+	grabCursor: true,
+	autoplay: {
+		delay: 7500,
+		disableOnInteraction: false,
+	},
+	loop: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+
+		768: {
+			slidesPerView: 2,
+		},
+		
+		991: {
+			slidesPerView: 3
+		}
+	}
+})
